09d2c5867376790fc5dfab195dda4fb3d91f5ee6
Eleasah Halsmer <eleasahjh@gmail.com>
2019-12-08 13:34:37 -0700

    Added input via date select. Number of days is rerendered inside rerender function

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 import {html, render} from 'lit-html'; 
 
-const targetDate = '2019-12-25';
-const days = Math.round((new Date(targetDate) - new Date()) / (1000 * 60 * 60 * 24)) // compare target with today, and convert from milliseconds to days
+const onTargetDateChange = (e) => {
+    rerender(e.target.value)
+}
 
-const template = html`
-    <p>
-        <span class=${days < 30 ? 'soon' : ''}>${days}</span>
-         days left until Christmas
-    </p>
-    `
+const targetDate = '2020-01-01';
 
-render(template, document.getElementById('my-app'));
+function rerender(targetDate) {
+    const days = Math.round((new Date(targetDate) - new Date()) / (1000 * 60 * 60 * 24)) // compare target with today, and convert from milliseconds to days
+    
+    const template = html`
+        <p>
+            <span class=${days < 30 ? 'soon' : ''}>${days}</span>
+             days left until holidays
+        </p>
+        <button>Click</button>
+    
+        <p>
+            My holidays start at
+            <input type="date" value=${targetDate} @change=${onTargetDateChange} />
+        </p>
+        `
+    
+    render(template, document.getElementById('my-app'));
+}
+
+rerender('2019-12-22')
